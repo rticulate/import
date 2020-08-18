b6d32bf021d816006d7beaf035797b921aadfe28
Magnus Thor Torfason <m@zulutime.net>
2020-08-18 00:15:42 +0000

    Trim irrelevant comment

--- tests/test_import/module_recursive/src/title_text_here.R
+++ tests/test_import/module_recursive/src/title_text_here.R
@@ -1,5 +1,3 @@
-# If import::from does not change the path to the dirname(path)
-# (like the chdir=TRUE in sys.source), this shouldn't work.
 import::here(.from=to_title.R, to_title)
 
 print_title_text = function(text){
